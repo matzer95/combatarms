e906095a9b60252847538a61921f8a71715f4c21
matzer95 <matzer.95@gmail.com>
2019-02-12 21:00:30 +0100

    update2

--- index.js
+++ index.js
@@ -12,4 +12,21 @@ client.on('message', message => {
 	if (message.content === '!gray_outside_a') message.reply('https://www.youtube.com/watch?v=ZkVDDrPZQj0');
 });
 
+client.on('message', function (user, userID, channelID, message, evt) {
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+       
+        args = args.splice(1);
+        switch(cmd) {
+            case 'hund':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'ne du'
+                });
+            break;
+         }
+     }
+});
+
 client.login(token).catch(err => console.log(err));
