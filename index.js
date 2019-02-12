e338934566aa4d657a7de61295f09a7435211cba
matzer95 <matzer.95@gmail.com>
2019-02-12 21:07:06 +0100

    update3

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ client.on('ready', () => {
 	console.log("I'm read to ban!");
 });
 
-//client.on('message', message => {
-//	if (message.content === '!sf_carnade') message.reply('https://www.youtube.com/watch?v=0nd11CmDdxE');
-//	if (message.content === '!docs') message.reply('http://pastebin.com/AGjHZKmj');
-//	if (message.content === '!gray_outside_a') message.reply('https://www.youtube.com/watch?v=ZkVDDrPZQj0');
-//});
+client.on('message', message => {
+	if (message.content === '!sf_carnade') message.reply('https://www.youtube.com/watch?v=0nd11CmDdxE');
+	if (message.content === '!docs') message.reply('http://pastebin.com/AGjHZKmj');
+	if (message.content === '!gray_outside_a') message.reply('https://www.youtube.com/watch?v=ZkVDDrPZQj0');
+});
 
-client.on('message', function (user, userID, channelID, message, evt) {
+/*client.on('message', function (user, userID, channelID, message, evt) {
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
@@ -27,6 +27,6 @@ client.on('message', function (user, userID, channelID, message, evt) {
             break;
          }
      }
-});
+});*/
 
 client.login(token).catch(err => console.log(err));
