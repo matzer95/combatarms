31c3934395c019442f1c2ab562f662236fefc67e
matzer95 <matzer.95@gmail.com>
2019-02-13 15:43:58 +0100

    1

--- index.js
+++ index.js
@@ -3,21 +3,30 @@ const client = new Discord.Client();
 const token = process.env.token;
 
 client.on('ready', () => {
-	console.log("I'm ready to ban!");
+	console.log("Beep Boop, I'm ready!");
 });
 
 client.on('message', message => {
-	if (message.content === '!info') message.channel.send("Type !docs or !'map_name'");
-	if (message.content === '!docs') message.reply("\n" +
+	// INFO
+	if (message.content === '!info') message.channel.send("Try !docs or !'map_name'");
+	// ALLE DOCS
+	if (message.content === '!docs') message.channel.send(
 		"GRAY HAMMER: https://docs.google.com/document/d/1txZIn904D2F9ZNe4lWhoGROZX4K85A4OmSIkZfGete0/edit?usp=sharing" + "\n" +
 		"POWER SURGE: https://docs.google.com/document/d/1iAD8b1Zcaymvqx1eaPLEMDSTn4yLtUDerclUeJ_QrNo/edit?usp=sharing" + "\n" +
 		"RATTLESNAKE: https://docs.google.com/document/d/1ApP13FWX_YlhjQlXuFnd7nGA73crY337Kt8_Ud00CbA/edit?usp=sharing" + "\n" +
 		"SHORT FUSE:  https://docs.google.com/document/d/1QL46WkgNSaC6J3vnnVXxm3_RohT70blv-I87Qt8M0Vs/edit?usp=sharing" + "\n" +
-		"SNOW VALLEY: https://docs.google.com/document/d/1jBo7Rw5eVvdei12IwowTgp6YyK9OhD_XfyCqhclR5Y4/edit?usp=sharing");
-	if ((message.content === '!shortfuse') || (message.content === '!short_fuse')) message.reply('https://docs.google.com/document/d/1QL46WkgNSaC6J3vnnVXxm3_RohT70blv-I87Qt8M0Vs/edit?usp=sharing');
-	if (message.content === '!gray_outside_a') message.reply('https://www.youtube.com/watch?v=ZkVDDrPZQj0');
-	if (message.content === '!sf_bluecar') message.reply('https://imgur.com/a/zMdzP');
-	if (message.content === '!sf_carnade') message.reply('https://www.youtube.com/watch?v=0nd11CmDdxE');
+		"SNOW VALLEY: https://docs.google.com/document/d/1jBo7Rw5eVvdei12IwowTgp6YyK9OhD_XfyCqhclR5Y4/edit?usp=sharing"
+		);
+	// EINZELNE DOCS
+	if ((message.content === '!grayhammer') || (message.content === '!gray_hammer')) message.channel.send('https://docs.google.com/document/d/1txZIn904D2F9ZNe4lWhoGROZX4K85A4OmSIkZfGete0/edit?usp=sharing');
+	if ((message.content === '!powersurge') || (message.content === '!power_surge')) message.channel.send('https://docs.google.com/document/d/1iAD8b1Zcaymvqx1eaPLEMDSTn4yLtUDerclUeJ_QrNo/edit?usp=sharing');
+	if ((message.content === '!rattlesnake') || (message.content === '!rattle')) message.channel.send('https://docs.google.com/document/d/1ApP13FWX_YlhjQlXuFnd7nGA73crY337Kt8_Ud00CbA/edit?usp=sharing');
+	if ((message.content === '!shortfuse') || (message.content === '!short_fuse')) message.channel.send('https://docs.google.com/document/d/1QL46WkgNSaC6J3vnnVXxm3_RohT70blv-I87Qt8M0Vs/edit?usp=sharing');
+	if ((message.content === '!snowvalley') || (message.content === '!snow_valley')) message.channel.send('https://docs.google.com/document/d/1jBo7Rw5eVvdei12IwowTgp6YyK9OhD_XfyCqhclR5Y4/edit?usp=sharing');
+	// EINZELNE NADES
+	if (message.content === '!gray_outside_a') message.channel.send('https://www.youtube.com/watch?v=ZkVDDrPZQj0');
+	if (message.content === '!sf_bluecar') message.channel.send('https://imgur.com/a/zMdzP');
+	if (message.content === '!sf_whitecar') message.channel.send('https://www.youtube.com/watch?v=0nd11CmDdxE');
 	});
 
 /*client.on('message', function (user, userID, channelID, message, evt) {
