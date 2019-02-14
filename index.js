f5a7bc634481547a75ab38cadf0439c189e40c73
matzer95 <matzer.95@gmail.com>
2019-02-14 19:30:54 +0100

    121

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-	if ((message.content.startsWith("!")) /*|| (message.content === 'This command does not exist.')*/) {
+	if (message.content.startsWith("!")) {
 		// INFO
 		if (message.content === '!info') message.channel.send("Try !docs or !'map_name'");
 		// ALLE DOCS
@@ -41,7 +41,7 @@ client.on('message', message => {
 			if (message.content === '!1v4mtp') message.channel.send('https://www.youtube.com/watch?v=dS-bbM_RNgw');
 			if (message.content === '!modemtap') message.channel.send('https://www.youtube.com/watch?v=mEK9k_2nH0M');
 			if (message.content === '!r4z') message.channel.send('https://www.youtube.com/watch?v=IfghPvgU1Zk');
-		} else message.channel.send('This command does not exist.');
+		} else if (1 === 1) message.channel.send('This command does not exist.');
 	} else if (message.member.user.id !== '544956570539393024') message.delete(1);
 });
 
