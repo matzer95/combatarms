a092a5851eca9eced47764ca27d63638de94ec72
MatzeR95 <matzer.95@gmail.com>
2019-12-17 19:58:45 +0100

    addremove

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ client.on('message', message => {
 		// LIST
 		else if (message.content === '!list') message.channel.send(list_of_commands);
 		// ROLE ASSIGN
-		else if (message.content === '!b') message.channel.send('a');
-		else if (message.content === '!a') message.member.addRole('655357708773621770');
+		else if (message.content === '!add_YT') message.member.addRole('655357708773621770');
+		else if (message.content === '!remove_YT') message.member.removeRole('655357708773621770');
 		// NOT EXIST COMMAND
 		else message.channel.send('This command does not exist.');
 	} else if (message.member.user.id !== '544956570539393024') message.delete(1);
